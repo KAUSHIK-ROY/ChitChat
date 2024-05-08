0ab48200093e6b8408060ab8aedb1f1e216be189
KAUSHIK-ROY <kroy33387@gmail.com>
2024-05-08 23:22:18 +0530

    Add Log Out button

--- src/Components/Friends/myProfile.jsx
+++ src/Components/Friends/myProfile.jsx
@@ -1,74 +1,124 @@
-import React, { useState } from 'react';
-import './myProfile.css';
-import AllChats from './AllChats.jsx';
+import React, { useState } from "react";
+import "./myProfile.css";
+import AllChats from "./AllChats.jsx";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faBars, faBomb, faChevronDown, faCircleInfo, faGear, faPenToSquare, faPhotoVideo, faSdCard, faSearch, faXmark} from '@fortawesome/free-solid-svg-icons';
-import { faBell, faStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowLeft,
+  faBars,
+  // faBomb,
+  faChevronDown,
+  faCircleInfo,
+  faGear,
+  faPenToSquare,
+  faPhotoVideo,
+  faSdCard,
+  faSearch,
+  // faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { faBell, faStar } from "@fortawesome/free-regular-svg-icons";
 
 export default function MyProfile() {
-
-  const[menu,setMenu] = useState(false);
-  let toggleMenu = ()=>{
-    setMenu(!menu)
-  }
+  const [menu, setMenu] = useState(false);
+  let toggleMenu = () => {
+    setMenu(!menu);
+  };
   return (
     <div className="friends">
-
       <div className="menulogo">
-      
-        <div className={`slideMenu ${menu? 'activeSlideMenu' : ''}` }>
+        <div className={`slideMenu ${menu ? "activeSlideMenu" : ""}`}>
           <div className="btn">
-            <button className='bar-btn' onClick={toggleMenu}>
-                <FontAwesomeIcon icon={faArrowLeft}/>
+            <button className="bar-btn" onClick={toggleMenu}>
+              <FontAwesomeIcon icon={faArrowLeft} />
             </button>
           </div>
           <div className="allList">
             <ul>
-              <li><div className='idiv'> <FontAwesomeIcon icon={faBomb}/></div><div className='iname'>My Profile           </div></li>
-              <li><div className='idiv'> <FontAwesomeIcon icon={faBell}/></div><div className='iname'>Notifications        </div></li>
-              <li><div className='idiv'> <FontAwesomeIcon icon={faPhotoVideo}/></div><div className='iname'>Images & videos</div></li>
-              <li><div className='idiv'> <FontAwesomeIcon icon={faSdCard}/></div><div className='iname'>Storage            </div></li>
-              <li><div className='idiv'> <FontAwesomeIcon icon={faGear}/></div><div className='iname'>Settings             </div></li>
-              <li><div className='idiv'> <FontAwesomeIcon icon={faCircleInfo}/></div><div className='iname'>About us       </div></li>
+              <li>
+                <div className="idiv">
+                  {" "}
+                  {/* <FontAwesomeIcon icon={faBomb} /> */}
+                  <img src=" " alt="" />
+                </div>
+                <div className="iname">My Profile </div>
+              </li>
+              <li>
+                <div className="idiv">
+                  {" "}
+                  <FontAwesomeIcon icon={faBell} />
+                </div>
+                <div className="iname">Notifications </div>
+              </li>
+              <li>
+                <div className="idiv">
+                  {" "}
+                  <FontAwesomeIcon icon={faPhotoVideo} />
+                </div>
+                <div className="iname">Images & videos</div>
+              </li>
+              <li>
+                <div className="idiv">
+                  {" "}
+                  <FontAwesomeIcon icon={faSdCard} />
+                </div>
+                <div className="iname">Storage </div>
+              </li>
+              <li>
+                <div className="idiv">
+                  {" "}
+                  <FontAwesomeIcon icon={faGear} />
+                </div>
+                <div className="iname">Settings </div>
+              </li>
+              <li>
+                <div className="idiv">
+                  {" "}
+                  <FontAwesomeIcon icon={faCircleInfo} />
+                </div>
+                <div className="iname">About us </div>
+              </li>
             </ul>
           </div>
-
+          <div className="logAcc">
+            <button className="switch-btn">Switch Account</button>
+            <div className="log"><button className="log-btn"><span>Log Out</span></button></div>
+          </div>
         </div>
-        <button className='bar-btn' onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars}/>
+        <button className="bar-btn" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
         </button>
-        <h1>ChitChat</h1>
+        
       </div>
-
 
       <div className="mychats">
         <div className="im">
-          <div className='msgarr'>
+          <div className="msgarr">
             <h2>Messages</h2>
-            <FontAwesomeIcon icon={faChevronDown} className='downarrow' />
+            <FontAwesomeIcon icon={faChevronDown} className="downarrow" />
           </div>
-          <div className='micn'>
+          <div className="micn">
             <FontAwesomeIcon icon={faPenToSquare} />
             <FontAwesomeIcon icon={faStar} />
           </div>
         </div>
         <div className="usearch">
-          <button className='sbtn'><FontAwesomeIcon icon={faSearch} /></button>
-          <input type="text" placeholder='Search'/>
+          <button className="sbtn">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+          <input type="text" placeholder="Search" />
         </div>
         <div className="allchats">
-          <AllChats/>
-          <AllChats/>
-          <AllChats/>
-          <AllChats/>
-          <AllChats/>
-          <AllChats/>
-          <AllChats/>
-          <AllChats/>
-          <AllChats/>
+          <AllChats />
+          <AllChats />
+          <AllChats />
+          <AllChats />
+          <AllChats />
+          <AllChats />
+          <AllChats />
+          <AllChats />
+          <AllChats />
         </div>
       </div>
     </div>
-  )
+  );
 }
