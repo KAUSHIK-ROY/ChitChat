afa225fc1e4716c6c5bc8dcfb900662d1d71385c
KAUSHIK-ROY <kroy33387@gmail.com>
2024-05-09 23:42:22 +0530

    Add Sign In page

---
+++ src/Components/Firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth, grtAuth} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNaYz5jBsaB1CZmNqWZyXJjFGjGgjMKQU",
+  authDomain: "chitchat-422817.firebaseapp.com",
+  projectId: "chitchat-422817",
+  storageBucket: "chitchat-422817.appspot.com",
+  messagingSenderId: "14729923125",
+  appId: "1:14729923125:web:be5126df1e1dace6553f7c",
+  measurementId: "G-H11T1BEXF5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth();
+export const db = getFirestore(app);
+export default app;
--- src/Components/LogIn/Login.jsx
+++ src/Components/LogIn/Login.jsx
@@ -1,67 +1,105 @@
-import './login.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+import "./login.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Login() {
+  const [form, setForm] = useState(true);
+
+  let toggleForm = () => {
+    setForm(!form);
+  };
+
   return (
-    <div className='lcontainer'>
-        <div className="logContainer">
+    <div className="lcontainer">
+      <div className="logContainer">
+        {form ? (
+          <>
             <div className="curved-shape"></div>
+
             <div className="form-box login">
-                <h2>Login</h2>
-                <form action="">
-                    <div className="input-box">
-                        <input type="text" required/>
-                        <label htmlFor="">Username</label>
-                        <FontAwesomeIcon icon={faUser} className='icn'/>
-                    </div>
-                    <div className="input-box">
-                        <input type="text" required/>
-                        <label htmlFor="">Password</label>
-                        <FontAwesomeIcon icon={faLock} className='icn'/>
-                    </div>
-                    <div className="input-box">
-                        <button className="btn1" type='submit'>Login</button>
-                    </div>
-                    <div className="regi-link">
-                        <p>Dont't have an account? <span>Sign up</span></p>
-                    </div>
-                </form>
+              <h2>Login</h2>
+              <form action="">
+                <div className="input-box">
+                  <input type="text" required />
+                  <label htmlFor="">Username</label>
+                  <FontAwesomeIcon icon={faUser} className="icn" />
+                </div>
+                <div className="input-box">
+                  <input type="text" required />
+                  <label htmlFor="">Password</label>
+                  <FontAwesomeIcon icon={faLock} className="icn" />
+                </div>
+                <div className="input-box">
+                  <button className="btn1" type="submit">
+                    Login
+                  </button>
+                </div>
+                <div className="regi-link">
+                  <p>
+                    Dont't have an account?{" "}
+                    <span onClick={toggleForm} className="SignUpLink">
+                      Sign up
+                    </span>
+                  </p>
+                </div>
+              </form>
             </div>
             <div className="info-content login">
-            <div className="logo"><h1>ChitChat</h1></div>
-                <h2>Welcome Back!</h2>
-                <p>Let's start a gossip.</p>
+              <div className="logo">
+                <h1>ChitChat</h1>
+              </div>
+              <h2>Welcome Back!</h2>
+              <p>Let's start a gossip.</p>
             </div>
+          </>
+        ) : (
+          <>
+            <div className="curved-shape2"></div>
             <div className="form-box register">
-                <h2>Register</h2>
-                <form action="">
-                    <div className="input-box">
-                        <input type="text" required/>
-                        <label htmlFor="">Username</label>
-                        <FontAwesomeIcon icon={faUser} className='icn'/>
-                    </div>
-                    <div className="input-box">
-                        <input type="text" required/>
-                        <label htmlFor="">Password</label>
-                        <FontAwesomeIcon icon={faLock} className='icn'/>
-                    </div>
-                    <div className="input-box">
-                        <button className="btn1" type='submit'>Register</button>
-                    </div>
-                    <div className="regi-link">
-                        <p>Dont't have an account? <span>Sign In</span></p>
-                    </div>
-                </form>
+              <h2>Register</h2>
+              <form action="">
+                <div className="input-box">
+                  <input type="text" required />
+                  <label htmlFor="">Username</label>
+                  <FontAwesomeIcon icon={faUser} className="icn" />
+                </div>
+                <div className="input-box">
+                  <input type="text" required />
+                  <label htmlFor="">Email</label>
+                  <FontAwesomeIcon icon={faUser} className="icn" />
+                </div>
+                <div className="input-box">
+                  <input type="password" required />
+                  <label htmlFor="">Set Password</label>
+                  <FontAwesomeIcon icon={faLock} className="icn" />
+                </div>
+                <div className="input-box">
+                  <button className="btn1" type="submit">
+                    Register
+                  </button>
+                </div>
+                <div className="regi-link">
+                  <p>
+                    Already have an account?{" "}
+                    <span onClick={toggleForm} className="SignInLink">
+                      Sign In
+                    </span>
+                  </p>
+                </div>
+              </form>
             </div>
             <div className="info-content register">
-                <h2>Welcome Back!</h2>
-                <p>Let's start a gossip.</p>
+              <div className="logo">
+                <h1>ChitChat</h1>
+              </div>
+              <h2>Thank You!</h2>
+              <p>Let's start a gossip.</p>
             </div>
-        </div>
-
+          </>
+        )}
+      </div>
     </div>
-  )
+  );
 }
