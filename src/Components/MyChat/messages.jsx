7527af546883d38fb32b6c44f28319e9e5620f17
KAUSHIK-ROY <kroy33387@gmail.com>
2024-05-10 03:26:53 +0530

    Arrange files

--- src/Components/MyChat/messages.jsx
+++ src/Components/MyChat/messages.jsx
@@ -1,24 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './message.css';
-import TypingDiv from './TypingDiv.jsx';
-import ChatNav from './ChatNav.jsx';
+import React, { useEffect, useRef, useState } from "react";
+import "./message.css";
+import TypingDiv from "./TypingDiv.jsx";
+import ChatNav from "./ChatNav.jsx";
 
 export default function Messages(props) {
+  const endRef = useRef(null);
 
-  const endRef = useRef(null)
-
-  useEffect(()=>{
-    endRef.current?.scrollIntoView({behavior : "smooth"})
-  } , [])
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
 
   return (
     <div>
       <div className="msg">
-        <ChatNav/>
+        <ChatNav />
         {/* <div class="spinner"></div> */}
         <div className="personalchat">
-          
-
 {/* demo */}
           <div className="message">
             <div className="text">
@@ -28,14 +25,21 @@ export default function Messages(props) {
           </div>
           <div className="message own">
             <div className="text">
-              <p>Lorem, ipsum dolor si modi laborum quisquam consectetur culpa repellat provident soluta voluptates ad, veniam, dolore mollitia quibusdam fugiat?</p>
+              <p>
+                Lorem, ipsum dolor si modi laborum quisquam consectetur culpa
+                repellat provident soluta voluptates ad, veniam, dolore mollitia
+                quibusdam fugiat?
+              </p>
               <span>1 min ago</span>
             </div>
           </div>
 
           <div className="message">
             <div className="text">
-              <p>Lorem ipsum dolor sit doloremque itaque eligendi error? Id assumenda dicta quasi quas?</p>
+              <p>
+                Lorem ipsum dolor sit doloremque itaque eligendi error? Id
+                assumenda dicta quasi quas?
+              </p>
               <span>1 min ago</span>
             </div>
           </div>
@@ -48,39 +52,60 @@ export default function Messages(props) {
 
           <div className="message">
             <div className="text">
-              <p>Lorem ipsum onsectetur adipisicing elit. Maxime iloremque itaque eligendquasi quas?</p>
+              <p>
+                Lorem ipsum onsectetur adipisicing elit. Maxime iloremque itaque
+                eligendquasi quas?
+              </p>
               <span>1 min ago</span>
             </div>
           </div>
           <div className="message own">
             <div className="text">
-              <p>Lorem, ipsum dolor sit amet consectetur  deleniti totam non eligendi sit modi laborum quisquam consectetur culpa repellat provident soluta voluptates ad, veniam, dolore mollitia quibusdam fugiat?</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur deleniti totam non
+                eligendi sit modi laborum quisquam consectetur culpa repellat
+                provident soluta voluptates ad, veniam, dolore mollitia
+                quibusdam fugiat?
+              </p>
               <span>1 min ago</span>
             </div>
           </div>
 
           <div className="message">
             <div className="text">
-              <p>Lorem ipsum elit. Maxime in impedit, doloremque iror? Id assumenda dicta uas?</p>
+              <p>
+                Lorem ipsum elit. Maxime in impedit, doloremque iror? Id
+                assumenda dicta uas?
+              </p>
               <span>1 min ago</span>
             </div>
           </div>
           <div className="message own">
             <div className="text">
-              <p>Lorem, consectetur adipisicing elit. Vel excepturi deleniti totam non eligendi sit modi laborum quisquam consectetur litia quibusdam fugiat?</p>
+              <p>
+                Lorem, consectetur adipisicing elit. Vel excepturi deleniti
+                totam non eligendi sit modi laborum quisquam consectetur litia
+                quibusdam fugiat?
+              </p>
               <span>1 min ago</span>
             </div>
           </div>
 
           <div className="message">
             <div className="text">
-              <p>Lorem ipsum dolor sit elit. Maxime in impedit, doloremque itaque eligendi error? Id assumenda dicta quasi quas?</p>
+              <p>
+                Lorem ipsum dolor sit elit. Maxime in impedit, doloremque itaque
+                eligendi error? Id assumenda dicta quasi quas?
+              </p>
               <span>1 min ago</span>
             </div>
           </div>
           <div className="message own">
             <div className="text">
-              <p>Lorem, laborum quisquam consectetur culpa repellat provident soluta voluptates ad, veniam, dolore mollitia quibusdam fugiat?</p>
+              <p>
+                Lorem, laborum quisquam consectetur culpa repellat provident
+                soluta voluptates ad, veniam, dolore mollitia quibusdam fugiat?
+              </p>
               <span>1 min ago</span>
             </div>
           </div>
@@ -99,16 +124,13 @@ export default function Messages(props) {
               <span>1 min ago</span>
             </div>
           </div>
-          
 
-          
 {/* demo end */}
           <div ref={endRef}></div>
         </div>
-        
-        <TypingDiv/>
 
+        <TypingDiv />
       </div>
     </div>
-  )
+  );
 }
--- src/Components/MyChat/typingDiv.jsx
+++ src/Components/MyChat/typingDiv.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
-import './typingDiv.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMicrophone, faPaperPlane, faPaperclip} from '@fortawesome/free-solid-svg-icons';
-import { faFaceSmile } from '@fortawesome/free-regular-svg-icons';
-import EmojiPicker from 'emoji-picker-react'; 
+import React, { useState } from "react";
+import "./typingDiv.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMicrophone,
+  faPaperPlane,
+  faPaperclip,
+} from "@fortawesome/free-solid-svg-icons";
+import { faFaceSmile } from "@fortawesome/free-regular-svg-icons";
+import EmojiPicker from "emoji-picker-react";
 
 export default function TypingDiv(props) {
   let [open, setOpen] = useState(false);
@@ -11,33 +15,44 @@ export default function TypingDiv(props) {
   const [data, setData] = useState("");
   const updateData = (event) => {
     setData(event.target.value);
-  }
+  };
 
   const handleSendMessage = () => {
-    setData('');
+    setData("");
   };
 
   return (
     <>
-    <div className='tdetails'>
-      <div className="tinp">
-        <FontAwesomeIcon icon={faFaceSmile} className='ticon' onClick={() => setOpen((prev) => !prev)}/>
-        <div className="picker">
-          <EmojiPicker open={open}/>
+      <div className="tdetails">
+        <div className="tinp">
+          <FontAwesomeIcon
+            icon={faFaceSmile}
+            className="ticon"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} />
+          </div>
+          <textarea
+            placeholder="Type a message..."
+            className="inpMsg"
+            rows="1"
+            value={data}
+            onChange={updateData}
+          ></textarea>
+          <div className="micclip">
+            <FontAwesomeIcon icon={faMicrophone} />
+            <FontAwesomeIcon icon={faPaperclip} />
+          </div>
+          {/* <FontAwesomeIcon icon={faCamera}/> */}
         </div>
-        <textarea placeholder='Type a message...' className='inpMsg' rows="1" value={data} onChange={updateData}></textarea>
-        <div className="micclip">
-          <FontAwesomeIcon icon={faMicrophone}/>
-          <FontAwesomeIcon icon={faPaperclip} />
-        </div>
-        {/* <FontAwesomeIcon icon={faCamera}/> */}
-      </div>  
 
-      <div className="send">
-        <button type='submit' onClick={handleSendMessage}><FontAwesomeIcon icon={faPaperPlane} /></button>
+        <div className="send">
+          <button type="submit" onClick={handleSendMessage}>
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </button>
+        </div>
       </div>
-
-    </div>
     </>
-  )
+  );
 }
